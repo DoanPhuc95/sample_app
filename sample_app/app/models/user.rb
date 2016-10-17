b1af088b1dacb48ebeb8a96780c6b27379305fe0
DoanPhuc95 <phucdm1901@gmail.com>
2016-10-17 03:47:15 -0700

    completed w6

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
 
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
-    return false if remember_digest.nil?
+  	return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
@@ -38,3 +38,4 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 end
+
--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -21,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161017094636) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",           default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
